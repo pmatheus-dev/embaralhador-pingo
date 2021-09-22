7d6b90e6562953559d1438a8ca68c184125e4785
p3dr0m4th3us123 <p3dr0m4th3us123@gmail.com>
2021-09-21 21:39:14 -0300

    Primeira Versão

---
+++ index.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Embaralhador</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <div class="box">
+        <h2>Embaralhador</h2>
+        <form action="/embaralhador/resultado.php" method="POST" >
+            <div class="inputBox">
+                <input type="number" name="numeroAlunos" id="numeroAlunos" min="0" max="2097152" required>
+                <label>Alunos<label>
+            </div>
+            <div class="inputBox">
+                <input type="number" name="numeroEquipes" id="numeroEquipes" min="0" max="2097152" required>
+                <label>Equipes</label>
+            </div>
+            <div class="inputBox">
+                <input type="text" name="disciplina" id="disciplina" required maxlength="64">
+                <label>Disciplina</label>
+            </div>
+            <div class="inputBox">
+                <input type="text" name="conteudo" id="conteudo" required maxlength="64">
+                <label>Conteúdo</label>
+            </div>
+            <div class="inputBox">
+                <input type="text" name="turma" id="turma" required maxlength="64">
+                <label>Turma</label>
+            </div>
+            <div class="inputBox">
+                <input type="text" name="descricao" id="descricao" required maxlength="256">
+                <label>Descrição</label>
+            </div>
+            <input type="submit" value="Gerar Equipes">
+        </form>
+    </div>
+</body>
+</html>
---
+++ resultado.php
@@ -0,0 +1,90 @@
+<?php
+date_default_timezone_set('America/Sao_Paulo');
+if(isset($_POST)){
+    $numeroAlunos = $_POST['numeroAlunos'];
+    $numeroEquipes = $_POST['numeroEquipes'];
+    $disciplina = $_POST['disciplina'];
+    $conteudo = $_POST['conteudo'];
+    $turma = $_POST['turma'];
+    $descricao = $_POST['descricao'];
+    $data = date("d/m/Y")." às ".date("H:i");
+
+    echo "<div class='box'>Sorteio realizado no dia $data<br><br>Disciplina: $disciplina <br>Conteúdo: $conteudo<br>Turma: $turma<br>Descrição: $descricao<br></div>";
+
+    $equipes = range(1, $numeroEquipes);
+    $alunos = range(1, $numeroAlunos);
+    shuffle($alunos);
+
+    $resto = $numeroAlunos % $numeroEquipes;
+    $alunosPorEquipe = intval($numeroAlunos / $numeroEquipes);
+    $sobraram = array_slice($alunos, count($alunos) - $resto, $resto);
+
+    if ($resto == 0){
+        for($i = 0; $i < $numeroEquipes; $i++) {
+            echo "<div class='box'>Equipe ".($i + 1)."<br>";
+            $array = array_slice($alunos, $alunosPorEquipe * $i, $alunosPorEquipe);
+            foreach ($array as $key => $a){
+                if ($key == count($array) - 1){
+                    echo $a."<br>";
+                }
+                else{
+                    echo $a.", ";
+                }
+                if ($key == count($array) - 1 ){
+                    echo "</div>";
+                }
+                
+            }
+        }
+    }
+    else{
+        for($i = 0; $i < $numeroEquipes; $i++) {
+            echo "<div class='box'>Equipe ".($i + 1)."<br>";
+            $array = array_slice($alunos, $alunosPorEquipe * $i, $alunosPorEquipe);
+            foreach ($array as $key => $a){
+                if ($key == count($array) - 1 AND $i > count($sobraram)){
+                    echo $a;
+                }
+                else{
+                    echo $a.", ";
+                }
+
+            }
+            echo $sobraram[0]."</div>";
+            $pop = array_shift($sobraram);
+        }
+    }
+}
+      
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado</title>
+    <link rel="stylesheet" href="./css/styleResultado.css">
+</head>
+<body>
+
+    <div style="text-align:center">
+        <button onclick="window.location='./'">Voltar</button>
+        <button onclick="salvar()">Salvar no WhatsApp</button>
+    </div>
+        
+    <script>
+        function salvar(){
+            var element = document.getElementsByClassName('box')
+            var texto = ""
+            for(var i = 0; i < element.length; i++){
+                texto += element[i].innerHTML + "%0A%0A"
+            }
+            var textoCorrigido = texto.replace(/<br>/g, "%0A")
+            window.location = `whatsapp:\/\/send?text=${textoCorrigido}`
+        }
+    </script>
+
+</body>
+</html>
